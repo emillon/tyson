15f4d49c633d257a2520bab440e76874b9bdd1bb
Etienne Millon <etienne.millon@gmail.com>
2011-03-18 14:42:28 +0100

    Add license

--- tyson.hs
+++ tyson.hs
@@ -1,3 +1,11 @@
+-- Copyright (c) 2011 Etienne Millon <etienne.millon@gmail.com>
+-------------------------------------------------------------------------------
+--                        "THE BEER-WARE LICENSE"
+-- <etienne.millon@gmail.com> wrote this file. As long as you retain this notice
+-- you can do whatever you want with this stuff. If we meet some day, and you
+-- think this stuff is worth it, you can buy me a beer in return.
+-------------------------------------------------------------------------------
+
 type ThreeOf a = (a, a, a)
 
 type CChar = ThreeOf (ThreeOf Char)
